2d434036c6604c4588eecb2e35c661e3988b65e3
dsarkar6575 <dipankarsarkar6575@gmail.com>
2025-04-29 04:47:02 +0530

    Add entity package

---
+++ src/main/java/com/scm/scm/entities/User.java
@@ -0,0 +1,63 @@
+package com.scm.scm.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "user")
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private String userId;
+
+    @Column(name = "user_name", nullable = true)
+    private String name;
+
+    @Column(unique = true, nullable = true)
+    private String email;
+
+    private String password;
+
+    @Column(length = 1000)
+    private String about;
+
+    @Column(length = 1000)
+    private String profilePic;
+
+    private String phoneNumber;
+
+    // Information
+    @Column(name = "enable")
+    private boolean enable = false;
+
+    private boolean emailVerified = false;
+    private boolean phoneVerified = false;
+
+    // SELF, GOOGLE, FACEBOOK, LINKEDIN, GITHUB, TWITTER
+    private Provider provider = Provider.SELF;
+    private String providerUserId;
+
+    // Add more fields
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Contact> contacts = new ArrayList<>();
+
+
+}
