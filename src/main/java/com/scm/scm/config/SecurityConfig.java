e3c87d69ed7496cb1daaeb38f99c618fa1ea4bcd
dsarkar6575 <dipankarsarkar6575@gmail.com>
2025-05-06 00:44:41 +0530

    security init

---
+++ src/main/java/com/scm/scm/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.scm.scm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+   
+  UserDetails user1=User
+  .withDefaultPasswordEncoder()
+  .username("admin123")
+  .password("admin123")
+  .roles("ADMIN","USER")
+  .build();
+  UserDetails user2=User
+  .withUsername("dsarkar")
+  .password("1234")
+  .build();
+
+
+  @Bean   
+  public UserDetailsService userDetailsService(){
+    return new InMemoryUserDetailsManager(user1);
+  }
+}
