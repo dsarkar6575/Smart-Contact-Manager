f50351a60966cf5f824f25d980b7aafce5c67ce1
dsarkar6575 <dipankarsarkar6575@gmail.com>
2025-05-20 13:19:31 +0530

    add contats process

---
+++ src/main/java/com/scm/scm/repsitories/ContactRepos.java
@@ -0,0 +1,22 @@
+package com.scm.scm.repsitories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.scm.scm.entities.Contact;
+import com.scm.scm.entities.User;
+
+@Repository
+public interface ContactRepos extends JpaRepository<Contact, String> {
+
+    //FIND BY USER 
+    List<Contact> findByUser(User user);
+    //customer query method
+    @Query("SELECT c FROM Contact c WHERE c.user.id = :userId")
+    List<Contact> findByUserId(@Param("userId") String userId);  
+    
+}
